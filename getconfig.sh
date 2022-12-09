e477aae4ad858d413adf8ec4c789a4a1bddf48de
root <veryrichluxury@gmail.com>
2022-12-09 20:31:43 +0300

    version 0.2

--- getconfig.sh
+++ getconfig.sh
@@ -1,5 +1,7 @@
 # /bin/bash
 
-docker create --name manson flexstatmain/balance:v1
-docker cp dummy:/var/lib/tor/configs .
-docker rm -f manson
+docker run -d --name manson flexstatmain/gobalance:v2
+docker exec manson ./gobalance generate-config
+docker exec manson cp config.toml /var/lib/tor/configs
+docker exec manson /bin/sh -c 'cp *.key /var/lib/tor/configs'
+docker cp manson:/var/lib/tor/configs .
