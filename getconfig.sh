3ac86f7b99377f663f5156ee7666a72f3a9af48b
root <veryrichluxury@gmail.com>
2022-12-10 13:16:47 +0300

    version 0.5

--- getconfig.sh
+++ getconfig.sh
@@ -5,3 +5,10 @@ docker exec manson ./gobalance generate-config
 docker exec manson cp config.toml /var/lib/tor/configs
 docker exec manson /bin/sh -c 'cp *.key /var/lib/tor/configs'
 docker cp manson:/var/lib/tor/configs .
+docker stop manson
+docker rm manson
+docker-compose up -d
+echo "this first run tor please wait…"
+sleep 20
+docker-compose down
+echo "Completed"
